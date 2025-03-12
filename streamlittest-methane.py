c78477ae48391625a35841e0fd4f2fa198f1962e
mikaelyanlab <mikaelyanlab@gmail.com>
2025-03-12 14:02:16 -0400

    Update streamlittest-methane.py

--- streamlittest-methane.py
+++ streamlittest-methane.py
@@ -1,20 +1,24 @@
-# Aram Mikaelyan, NCSU, March 12, 2025
-#
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
 # Define the model parameters
-def methane_oxidation(C, t, g_s, Vmax, Km, Pi, J_ETC, n_MMO, H_CH4, k_L):
+def methane_oxidation(C, t, g_s, Vmax, Km, Pi, J_ETC, n_MMO, T, k_L):
     C_atm = 1.8  # Atmospheric methane concentration (ppm)
     D_CH4 = 0.02  # Diffusion coefficient of methane
     d = 10e-6  # Distance in meters
     Y_NADH = 2  # NADH yield per electron
     k_MeOH = 0.05  # Methanol oxidation rate
     P_atm = 1.0  # Atmospheric pressure (atm)
+    H_0 = 1.4  # Henry's Law constant at reference temperature (25C)
+    alpha = 0.02  # Temperature sensitivity coefficient
+    beta = 0.01  # Osmotic effect coefficient
     
     C_int, C_cyt, CH3OH = C  # Unpacking state variables
+    
+    # Adjust Henry's Law constant based on temperature and osmolarity
+    H_CH4 = H_0 * np.exp(-alpha * (T - 25)) * (1 - beta * Pi)
     
     # Gas exchange through stomata
     P_CH4 = g_s * (C_atm / P_atm)  # Partial pressure of CH4
@@ -46,13 +50,13 @@ Km = st.sidebar.slider("Methane Affinity (Km)", 0.1, 2.0, 0.5)
 Pi = st.sidebar.slider("Cytosolic Osmolarity (%)", 0, 100, 50)
 J_ETC = st.sidebar.slider("Electron Transport (J_ETC)", 0.1, 3.0, 1.5)
 n_MMO = st.sidebar.slider("Number of sMMO Molecules per Cell", 1, 10000, 1000)
-H_CH4 = st.sidebar.slider("Methane Henry's Law Constant (H_CH4, mmol/L·atm)", 0.5, 2.0, 1.4)
+T = st.sidebar.slider("Temperature (°C)", 5, 45, 25)
 k_L = st.sidebar.slider("Mass Transfer Coefficient (k_L, m/s)", 0.001, 0.1, 0.01)
 
 # Solve ODEs
 time = np.linspace(0, 100, 500)
 C0 = [0.5, 0.2, 0.1]  # Initial concentrations
-sol = odeint(methane_oxidation, C0, time, args=(g_s, Vmax, Km, Pi, J_ETC, n_MMO, H_CH4, k_L))
+sol = odeint(methane_oxidation, C0, time, args=(g_s, Vmax, Km, Pi, J_ETC, n_MMO, T, k_L))
 
 # Plot results
 fig, ax = plt.subplots()
@@ -67,6 +71,7 @@ st.pyplot(fig)
 
 # Display equations
 st.sidebar.markdown("### Model Equations")
+st.sidebar.latex(r"H_{CH_4} = H_0 \cdot e^{-\alpha (T - 25)} \cdot (1 - \beta \Pi)")
 st.sidebar.latex(r"P_{CH_4} = g_s \cdot \frac{C_{atm}}{P_{atm}}")
 st.sidebar.latex(r"C_{cyt, eq} = H_{CH_4} \cdot P_{CH_4}")
 st.sidebar.latex(r"J_{CH_4} = k_L \cdot (C_{cyt, eq} - C_{cyt})")
