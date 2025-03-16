4877169cf77cb4fb9980cdcf70d1a59774bb73de
mikaelyanlab <mikaelyanlab@gmail.com>
2025-03-16 00:07:38 -0400

    Update cowpatec.py

--- cowpatec.py
+++ cowpatec.py
@@ -1,4 +1,4 @@
-import streamlit as st 
+import streamlit as st
 import numpy as np
 import plotly.graph_objects as go
 
@@ -21,15 +21,12 @@ NE_milk = 3  # Energy per kg of Milk
 
 # Constants for carbon partitioning (g/day)
 C_Intake = 6000  # Carbon Intake
-C_Fecal = 1800  # 30% of Carbon Intake lost as feces
-C_Urinary = 300  # Urinary Carbon Loss (~5% of Intake)
-C_CO2 = 2700  # 45% Respired CO2
+C_Fecal = 1200  # Fecal Carbon Loss
+C_Urinary = 202  # Urinary Carbon Loss
+C_CO2 = 1755  # Respired CO2
 C_Maintenance = 300  # Carbon for Maintenance
 C_Lactation = 200  # Carbon for Milk
 C_milk = 5  # Carbon per kg of Milk
-
-# Methane Carbon Loss (9% of intake)
-C_CH4 = C_Intake * 0.09
 
 # Energy functions
 def net_energy(GE, FE, CH4, UE, HI):
@@ -42,8 +39,8 @@ def milk_production_energy(NE, NEl, NE_milk):
     return (NE - NEl) / NE_milk  # Allow negative values
 
 # Carbon functions
-def net_carbon(C_Intake, C_Fecal, C_CH4, C_Urinary, C_CO2):
-    return C_Intake - (C_Fecal + C_CH4 + C_Urinary + C_CO2)
+def net_carbon(C_Intake, C_Fecal, CH4, C_Urinary, C_CO2):
+    return C_Intake - (C_Fecal + CH4 + C_Urinary + C_CO2)
 
 def weight_gain_carbon(C_Net, C_Maintenance, k_g):
     return k_g * (C_Net - C_Maintenance)  # Allow negative values
@@ -56,7 +53,7 @@ NE = net_energy(GE, FE, CH4, UE, HI)
 BW_gain_energy = weight_gain_energy(NE, MEm, k_g)
 Milk_Yield_energy = milk_production_energy(NE, NEl, NE_milk)
 
-C_Net = net_carbon(C_Intake, C_Fecal, C_CH4, C_Urinary, C_CO2)
+C_Net = net_carbon(C_Intake, C_Fecal, CH4, C_Urinary, C_CO2)
 BW_gain_carbon = weight_gain_carbon(C_Net, C_Maintenance, k_g)
 Milk_Yield_carbon = milk_production_carbon(C_Net, C_Lactation, C_milk)
 
@@ -65,7 +62,7 @@ energy_labels = ["Gross Energy", "Fecal Loss", "Urinary Loss", "Heat Increment",
 energy_values = [GE, -FE, -UE, -HI, -CH4]
 
 carbon_labels = ["Carbon Intake", "Fecal Loss", "Urinary Loss", "Respired CO2", "Methane Loss"]
-carbon_values = [C_Intake, -C_Fecal, -C_Urinary, -C_CO2, -C_CH4]
+carbon_values = [C_Intake, -C_Fecal, -C_Urinary, -C_CO2, -CH4]
 
 # Stacked net energy bar
 fig_energy = go.Figure()
@@ -73,7 +70,7 @@ fig_energy.add_trace(go.Bar(
     x=energy_labels,
     y=energy_values,
     marker_color=["blue", "red", "red", "red", "red"],
-    name="Total Gross Energy"
+    name="Energy Partitioning"
 ))
 fig_energy.add_trace(go.Bar(
     x=["Net Energy"],
@@ -95,7 +92,7 @@ fig_carbon.add_trace(go.Bar(
     x=carbon_labels,
     y=carbon_values,
     marker_color=["blue", "red", "red", "red", "red"],
-    name="Total Carbon Intake"
+    name="Carbon Partitioning"
 ))
 fig_carbon.add_trace(go.Bar(
     x=["Net Carbon"],
