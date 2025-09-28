f3ac05161bce98429b54a577439db36922a8d8ca
mikaelyanlab <mikaelyanlab@gmail.com>
2025-09-28 08:07:59 -0400

    Update final_riggle_pheno.py

--- final_riggle_pheno.py
+++ final_riggle_pheno.py
@@ -279,8 +279,8 @@ if st.button("Run Sensitivity Analysis"):
         yaxis_title="Parameter",
         xaxis=dict(
             tickmode='array',
-            tickvals=[i * 5 for i in range(0, 20, 5)],  # 0, 5, 10, ..., 95
-            ticktext=["0%", "25%", "50%", "75%", "100%"]  # Map to 0-100% percentiles
+            tickvals=[i * 5 for i in range(0, 100, 1)],  # 0, 5, 10, ..., 95
+            ticktext=["0%", "100%"]  # Map to 0-100% percentiles
         )
     )
     st.plotly_chart(fig_heatmap)
